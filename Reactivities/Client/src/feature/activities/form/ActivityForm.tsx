7c2029e5ba3db6b9d1d8f826b7afb34725cd9699
yverma5 <yashu15.1993@gmail.com>
2025-12-19 00:21:10 +0530

    Updated ActivityForm.tsx by removing import which was not used

--- Reactivities/Client/src/feature/activities/form/ActivityForm.tsx
+++ Reactivities/Client/src/feature/activities/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
-import { Activity, type FormEvent } from "react";
+import { type FormEvent } from "react";
 
 type Props={
     activity?: Activity
@@ -16,7 +16,7 @@ export default function ActivityForm({activity,closeForm,submitForm}:Props) {
     formData.forEach((value,key)=>{
         data[key]=value;
     });
-    
+
     if(activity) data.id=activity.id
 
     submitForm(data as unknown as Activity)
